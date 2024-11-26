e5174e433a4d3e1ee29ab986c024a8a1fea7bdec
HeroLeam <thiagoleam@gmail.com>
2024-11-25 22:01:38 -0300

    Updae

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,7 +1,7 @@
-const theDate = 17;
-const theMounth = "dec";
-const theHour = "16:48:00";
-const theYear = 2024;
+const theDate = 1;
+const theMounth = "jan";
+const theHour = "00:00:00";
+const theYear = 2025;
 const dateString = `${theMounth} ${theDate}, ${theYear} ${theHour}`;
 
 var deadline = new Date(dateString);
